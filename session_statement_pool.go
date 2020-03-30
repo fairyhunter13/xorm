356c510de2358067becd0fd74089e47f787dd03a
fairyhunter13 <fairyhunter13@gmail.com>
2020-03-30 14:41:48 +0700

    - Enhancing memory and gc optimization in session initialization :zap: :sparkles: - Enhancing optimization when reused global cache prepared statement :zap: :sparkles:

---
+++ session_statement_pool.go
@@ -0,0 +1,37 @@
+package xorm
+
+import "sync"
+
+var sessionPool = &sync.Pool{
+	New: func() interface{} {
+		return new(Session)
+	},
+}
+
+// Reset all the session to the initial states
+func (session *Session) Reset() {
+	session.engine = nil
+	session.tx = nil
+	session.statement = nil
+
+	session.isAutoCommit = false
+	session.isCommitedOrRollbacked = false
+	session.isAutoClose = false
+	session.isClosed = false
+	session.prepareStmt = false
+	session.autoResetStatement = false
+
+	session.afterInsertBeans = make(map[interface{}]*[]func(interface{}), 0)
+	session.afterUpdateBeans = make(map[interface{}]*[]func(interface{}), 0)
+	session.afterDeleteBeans = make(map[interface{}]*[]func(interface{}), 0)
+
+	session.beforeClosures = session.beforeClosures[:0]
+	session.afterClosures = session.afterClosures[:0]
+	session.afterProcessors = session.afterProcessors[:0]
+
+	session.lastSQL = session.lastSQL[:0]
+	session.lastSQLArgs = session.lastSQLArgs[:0]
+
+	session.ctx = nil
+	session.sessionType = engineSession
+}
